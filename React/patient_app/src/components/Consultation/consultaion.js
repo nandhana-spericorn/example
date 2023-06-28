cc24c3a78b4648843ec8ed1a8a0cd45cd45d2d31
nandhana-speri <nandhana@spericorn.com>
2023-06-28 15:17:43 +0530

    fifth commit

--- React/patient_app/src/components/Consultation/consultaion.js
+++ React/patient_app/src/components/Consultation/consultaion.js
@@ -10,19 +10,10 @@ const Consultation = () => {
           <div class="form-group a">
             <label for="name">Nom</label>
 
-            <label for="name">Nom</label>
-            <label for="name">Nom</label>
-            <label for="name">Nom</label>
-            <label for="name">Nom</label>
-            <label for="name">Nom</label>
-            <label for="name">Nom</label>
-
-
             <input id="name" type="text" />
           </div>
 
           <div class="form-group b">
-            <label for="first-name">Prénom</label>
             <label for="first-name">Prénom</label>
             <input id="first-name" type="text" />
           </div>
